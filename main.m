29efe65101d19801b01d656bb57f525dd115afba
Trong Quy Phan <p.trongquy@gmail.com>
2018-04-25 12:02:35 +0200

    Update

--- main.m
+++ main.m
@@ -1,22 +1,32 @@
 clear; close all;
 tic
-InputImageName = 'test3.jpg';
+InputImageName = 'original.jpg';
 WindowSize = 20;
 
-inImg = im2double(imread(InputImageName));
-% inImg(134:180,168:207,:) = 1;
-H = [134 180;168 207];
+originImg = im2double(imread(InputImageName));
+inImg = originImg;
+
+%% set black spot of test1.jpg
+inImg(134:180,165:208,:) = 1; % place a white spot at the target location
+H = [134 180;165 208];
+
+%% set black spot of test2.jpg
+% inImg(228:234,87:120,:) = 1; % place a white spot at the target location
+% H = [228 234;87 120];
+
+%% Inpainting
+paintedImg = inImg;
 
 all_patches = find_patches(inImg,H,WindowSize);
-all_patches = all_patches(1:200:end);
+all_patches = all_patches(1:200:end); % discard some patches otherwise too many 
 
-for raw = 134:180
-    for column = 168:168
+for raw = 134:134
+    for column = 165:208
         p_position = [raw,column]
-        Wi_set = find_Wi_Set(inImg,p_position,WindowSize);
+        Wi_set = find_Wi_Set_up(inImg,p_position,WindowSize);
         Vi_param = find_Vi_param_set(Wi_set,all_patches,p_position,WindowSize);
         wi_coef = [Vi_param.wi];
-        Ci_coef = reshape([Vi_param.color],3,WindowSize^2);
+        Ci_coef = reshape([Vi_param.color],3,WindowSize);
         product = (wi_coef.*Ci_coef)';
         C = sum(product)/sum(wi_coef);
         inImg(p_position(1),p_position(2),:) = C;
@@ -24,6 +34,10 @@ for raw = 134:180
 end
 toc
 
-%%
+%% Display results
 figure
-imshow(inImg);
+subplot(1,2,1)
+imshow(paintedImg)
+subplot(1,2,2)
+imshow(inImg)
+
