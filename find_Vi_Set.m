ff6ce4e718cc30d16df642765b1605edab86a21f
Trong Quy Phan <p.trongquy@gmail.com>
2018-04-29 22:31:10 +0200

    test

--- find_Vi_Set.m
+++
@@ -1,5 +0,0 @@
-function Vi = find_Vi_Set(Wi_set,all_patches,p_position,WindowSize)
-Vi = repmat(struct('patch',zeros(WindowSize^2,3),'wi',zeros(1),'color',zeros(3,1)),1,length(Wi_set));
-parfor i = 1:length(Wi_set)
-    Vi(i) = find_Vi(Wi_set(i),all_patches,p_position); 
-end
