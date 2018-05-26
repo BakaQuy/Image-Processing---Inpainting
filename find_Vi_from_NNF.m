814c18cdbc396f2152db73b250576006c4cf4af9
Lucanoel <luck3596@hotmail.be>
2018-05-26 11:59:26 +0200

    Add files via upload

--- find_Vi_from_NNF.m
+++ find_Vi_from_NNF.m
@@ -6,9 +6,7 @@ y2 = NNF(x1,y1,2);
 [m,n,~]=size(NNF);
 % disp(size(NNF))
 Window = (sqrt(length(Wi.patch)));
-[ min_x, min_y, max_x, max_y ]=limit_patch( x2, y2, n, m, Window );
-% disp(min_x)
-% disp(max_x)
+[ min_x, min_y, max_x, max_y ]=limit_patch( x2, y2, m, n, Window );
 Vi.patch = reshape(inImg(min_x:max_x,min_y:max_y,:),length(Wi.patch),3);
 Similitude = ComputeSim(Wi.patch,Vi.patch);
 Vi.wi = 1.3^(-norm([x1,y1]-[x2,y2]))*Similitude;
